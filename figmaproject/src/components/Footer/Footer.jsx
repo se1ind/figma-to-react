99f22b16890dcef600eb2a40045d3b39972f3bbe
se1ind <dulunduselin@gmail.com>
2024-11-24 22:48:15 +0300

    blog,testimonials and footer

--- figmaproject/src/components/Footer/Footer.jsx
+++ figmaproject/src/components/Footer/Footer.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import "./Footer.css";
+import {instagramLogo,twitterLogo,facebookLogo} from "../../assets";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+  <footer className="black">
+    <div className="wrapper">
+      <div className="content-container">
+        <div className="links">
+          <a href="#" className="logo">
+            Sound<span className="red">Dzign</span>
+          </a>
+           <div className="social-icons">
+            <a href="#">
+             <img src={facebookLogo} alt="" />
+            </a>
+            <a href="#">
+             <img src={twitterLogo} alt="" />
+            </a>
+            <a href="#">
+             <img src={instagramLogo} alt="" />
+            </a>
+
+           </div>
+           <div className="copyright">
+            This Website is designed by GTCoding Ⓒ 2023
+           </div>
+        </div>
+        <div className="links">
+          <h3>Quick Links</h3>
+          <ul>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+        <div className="links">
+          <h3>Contact Us</h3>
+          <ul>
+            <li>
+              <a href="#">contact@email.com</a>
+            </li>
+            <li>
+              <a href="#">+1 999 9999 999</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+
+    </div>
+     </footer>
   )
 }
 
